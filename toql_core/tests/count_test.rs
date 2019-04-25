d5f717417cedf6f8dba9a639c167a1b751c79a55
Roy Ganz <roy.ganz@glanis.ch>
2019-04-25 07:18:38 +0200

    Added test

---
+++ toql_core/tests/count_test.rs
@@ -0,0 +1,49 @@
+
+use toql_core::query_parser::QueryParser;
+use toql_core::sql_builder::SqlBuilder;
+use toql_core::sql_mapper::MapperOptions;
+use toql_core::sql_mapper::SqlMapper;
+
+fn setup_mapper() -> SqlMapper {
+    let mut mapper = SqlMapper::new("User");
+    mapper
+        .join("book", "JOIN Book b ON (id = b.id)")
+        .map_field("id", "id")
+        .map_field("username", "username")
+        .map_field("book_id", "b.id");
+    mapper
+}
+
+#[test]
+fn count_simple() {
+    let mapper = setup_mapper();
+    let query = QueryParser::parse("*, book_id eq 1").unwrap();
+    let result = SqlBuilder::new().build_count(&mapper, &query).unwrap();
+
+    assert_eq!("SELECT 1 FROM User", result.to_sql());
+}
+#[test]
+fn count_filter() {
+    let mut mapper = setup_mapper();
+    mapper.alter_field("book_id", "b.id", MapperOptions::new().count_filter(true));
+    let query = QueryParser::parse("*,book_id eq 1").unwrap();
+    let result = SqlBuilder::new().build_count(&mapper, &query).unwrap();
+    assert_eq!(
+        "SELECT 1 FROM User JOIN Book b ON (id = b.id) WHERE b.id = ?",
+        result.to_sql()
+    );
+    assert_eq!(*result.params(), ["1"]);
+}
+#[test]
+fn count_select() {
+    let mut mapper = setup_mapper();
+    mapper.alter_field("id", "id", MapperOptions::new().count_select(true));
+    mapper.alter_field("book_id", "b.id", MapperOptions::new().count_filter(true));
+    let query = QueryParser::parse("*,book_id eq 1").unwrap();
+    let result = SqlBuilder::new().build_count(&mapper, &query).unwrap();
+    assert_eq!(
+        "SELECT id FROM User JOIN Book b ON (id = b.id) WHERE b.id = ?",
+        result.to_sql()
+    );
+    assert_eq!(*result.params(), ["1"]);
+}
