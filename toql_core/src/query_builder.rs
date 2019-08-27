bd2e1356581ede4464fb1327fb029dba23c7b023
roy-ganz <roy.ganz@glanis.biz>
2019-08-27 11:38:47 +0200

    Refractor code

---
+++ toql_core/src/query_builder.rs
@@ -0,0 +1,11 @@
+
+
+//! Trait to associate a field type provider with a struct.
+
+/// Used by code produced from Toql derive.
+pub trait QueryFields {
+    type FieldsType;
+    
+    fn fields() -> Self::FieldsType; 
+    fn fields_from_path(path: String) -> Self::FieldsType;
+}
