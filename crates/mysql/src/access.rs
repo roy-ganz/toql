fb9fa9531a88989fea564c2a9f73db0fb0082d39
roy-ganz <roy.ganz@glanis.biz>
2020-11-30 16:00:18 +0100

    Cleanup

--- crates/mysql/src/access.rs
+++ crates/mysql/src/access.rs
@@ -1,8 +1,6 @@
 #[macro_export]
 macro_rules! mysql_row_try_get {
-    // `()` indicates that the macro takes no argument.
     ($var: tt, $index: expr) => {
-        // The macro will expand into the contents of this block.
         $var.get_opt($index).unwrap()
     };
 }
