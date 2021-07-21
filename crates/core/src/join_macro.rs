642e5175800593e272d8a5f8234094c935f25d20
roy-ganz <roy.ganz@glanis.biz>
2021-07-21 10:16:27 +0200

    TryJoin path

--- crates/core/src/join_macro.rs
+++ crates/core/src/join_macro.rs
@@ -2,12 +2,12 @@
 #[macro_export]
 macro_rules! join {
     ($x: expr) => {
-        $x.try_join_or(toql::none_error!())?
+        toql::try_join::TryJoin::try_join_or($x, toql::none_error!())?
     };
 }
 #[macro_export]
 macro_rules! rjoin {
     ($x: expr) => {
-        $x.as_ref().try_join_or(toql::none_error!())?
+        toql::try_join::TryJoin::try_join_or($x.as_ref(), toql::none_error!())?
     };
 }
